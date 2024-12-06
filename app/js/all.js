493cd69693bdce602756414d39dbaed5eb3eeeef
Micko4ka <mariia.maleryk.pm.2023@lpnu.ua>
2024-12-07 00:58:46 +0200

    questionnaire

--- app/js/all.js
+++ app/js/all.js
@@ -1,20 +1,20 @@
-// Get the scroller and arrow buttons
-const scroller = document.querySelector('.scroller');
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
-
-// Amount to scroll per button click
-const scrollAmount = 500; // Adjust for your layout
-
-// Scroll left
-leftArrow.addEventListener('click', () => {
-    scroller.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-});
-
-// Scroll right
-rightArrow.addEventListener('click', () => {
-    scroller.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-});
+// // Get the scroller and arrow buttons
+// const scroller = document.querySelector('.scroller');
+// const leftArrow = document.querySelector('.left-arrow');
+// const rightArrow = document.querySelector('.right-arrow');
+//
+// // Amount to scroll per button click
+// const scrollAmount = 500; // Adjust for your layout
+//
+// // Scroll left
+// leftArrow.addEventListener('click', () => {
+//     scroller.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+// });
+//
+// // Scroll right
+// rightArrow.addEventListener('click', () => {
+//     scroller.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+// });
 
 // Скрипт для активації активного посилання при прокручуванні
 const links = document.querySelectorAll("nav ul li a");
