367823a692fdd91a055d0b84bca1654fcb1bb109
Micko4ka <mariia.maleryk.pm.2023@lpnu.ua>
2024-12-07 17:23:30 +0200

    commit

--- app/js/hide-arrows.js
+++ app/js/hide-arrows.js
@@ -4,6 +4,23 @@
     const arrows = document.querySelector('.arrows');
 
     if (images.length <= 3) {
+        scroller.classList.add('few-images');
         arrows.classList.add('hidden');
+    }
+
+    if (images.length === 0) {
+        scroller.classList.add('hidden');
+    }
+
+    if (images.length === 3) {
+        scroller.classList.add('three');
+    }
+
+    if (images.length === 2) {
+        scroller.classList.add('two');
+    }
+
+    if (images.length === 1) {
+        scroller.classList.add('one');
     }
 })();
